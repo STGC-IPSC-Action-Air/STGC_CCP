55577694b172462e52e2569a05bb2ba098ad724a
jkh675 <rluo2018@gmail.com>
2023-03-18 21:30:12 +0800

    [feat]: top bar impl

--- src/web_src/src/app/app-routing.module.ts
+++ src/web_src/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	bootstrap: [AppComponent],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/web_src/src/global.ts
@@ -0,0 +1,4 @@
+export const GlobalVariable = Object.freeze({
+	SITE_NAME: "STGC CCP", // Site name
+	//... more of your variables
+});
